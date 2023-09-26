4b7264fe83cb9d339d57ea9a717d6607cf859db7
zolppy <gabriel.lcifba@gmail.com>
2023-09-26 13:45:35 -0300

    bot01.sh fez este upload

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,33 +1,85 @@
-const redContainer = document.querySelector('#red');
-const greenContainer = document.querySelector('#green');
-const blueContainer = document.querySelector('#blue');
-const copyButton = document.querySelector('#copy-btn');
+const RSliderElement = document.querySelector('#r-slider');
+const GSliderElement = document.querySelector('#g-slider');
+const BSliderElement = document.querySelector('#b-slider');
+const RGBOutputCopyButton = document.querySelector('#rgb-output-copy-button');
+const HexOutputCopyButton = document.querySelector('#hex-output-copy-button');
 
-const colors = () => {
-  let redValue = redContainer.value;
-  let greenValue = greenContainer.value;
-  let blueValue = blueContainer.value;
-  const bodyElement = document.querySelector('body');
-  const outputElement = document.querySelector('#output');
-  const copyIconElement = document.querySelector('#copy-icon');
+/* Se o valor possuir um dígito, receberá 0 antes dele, passando a ter dois */
+const forTwoDigits = (value) => value < 10 ? '0' + value : value;
 
+/* Atualiza o código de cor RGB da saída */
+const updateRGBOutput = (RValue, GValue, BValue) => {
+  const RGBOutputElement = document.querySelector('#rgb-output');
+  const RGBOutputCopyIconElement = document.querySelector('#rgb-output-copy-icon');
 
-  bodyElement.style.backgroundColor = `rgb(${redValue}, ${greenValue}, ${blueValue})`;
-  outputElement.value = `rgb(${redValue}, ${greenValue}, ${blueValue})`;
-
-  copyIconElement.classList.replace('bi-clipboard-check', 'bi-clipboard');
+  RGBOutputElement.value = `rgb(${RValue}, ${GValue}, ${BValue});`;
+  RGBOutputCopyIconElement.classList.replace('bi-clipboard-check', 'bi-clipboard');
 }
 
-const copy = () => {
-  const outputElement = document.querySelector('#output');
-  const copyIconElement = document.querySelector('#copy-icon');
-  let text = outputElement.value;
+/* Atualiza o código de cor hexadecimal da saída */
+const updateHexOutput = (RValue, GValue, BValue) => {
+  const HexOutputElement = document.querySelector('#hex-output');
+  const HexOutputCopyIconElement = document.querySelector('#hex-output-copy-icon');
 
-  outputElement.focus();
+  /* As letras são minúsculas, por padrão, mas pode ser utilizado ".toUpperCase()" para transformá-las */
+  let RHexValue = forTwoDigits(RValue.toString(16)); /* 00 a ff*/
+  let GHexValue = forTwoDigits(GValue.toString(16));
+  let BHexValue = forTwoDigits(BValue.toString(16));
+
+  HexOutputElement.value = `#${RHexValue}${GHexValue}${BHexValue};`;
+  HexOutputCopyIconElement.classList.replace('bi-clipboard-check', 'bi-clipboard');
+}
+
+/* Atualiza a cor de fundo da página, de acordo com a combinação dos sliders */
+const updateBackgroundColor = (RValue, GValue, BValue) => {
+  const bodyElement = document.querySelector('body');
+
+  bodyElement.style.backgroundColor = `rgb(${RValue}, ${GValue}, ${BValue})`;
+}
+
+const getColorValues = () => {
+  /* Conversão simples de string para número. É semelhante a usar "a = parseInt(b);" */
+  let RValue = +RSliderElement.value;
+  let GValue = +GSliderElement.value;
+  let BValue = +BSliderElement.value;
+  
+  updateBackgroundColor(RValue, GValue, BValue);
+  updateRGBOutput(RValue, GValue, BValue);
+  updateHexOutput(RValue, GValue, BValue);
+}
+
+/* Copia código de cor RGB à área de transferência */
+const copyRBG = () => {
+  const RGBOutputElement = document.querySelector('#rgb-output');
+  const RGBOutputCopyIconElement = document.querySelector('#rgb-output-copy-button');
+  let text = RGBOutputElement.value;
+
+  /* Necessário para que a cópia funcione */
+  RGBOutputElement.focus(); /* Só é possível utilizar "focus" em inputs */
+
+  /* Gera uma promessa, se for cumprida, faça algo, caso contrário, capture o erro e imprima-o no console */
+  navigator.clipboard.writeText(text)
+  .then(() => {
+    RGBOutputCopyIconElement.classList.replace('bi-clipboard', 'bi-clipboard-check');
+    alert('Copiado para área de transferência!');
+  })
+  /* Não há tratamento de erros */
+  .catch((error) => {
+    console.error('Falha ao copiar:', error);
+    alert('Falha ao copiar para a área de transferência.');
+  });
+}
+
+const copyHex = () => {
+  const HexOutputElement = document.querySelector('#hex-output');
+  const HexOutputCopyIconElement = document.querySelector('#hex-output-copy-button');
+  let text = HexOutputElement.value;
+
+  HexOutputElement.focus();
 
   navigator.clipboard.writeText(text)
   .then(() => {
-    copyIconElement.classList.replace('bi-clipboard', 'bi-clipboard-check');
+    HexOutputCopyIconElement.classList.replace('bi-clipboard', 'bi-clipboard-check');
     alert('Copiado para área de transferência!');
   })
   .catch((error) => {
@@ -36,7 +88,9 @@ const copy = () => {
   });
 }
 
-redContainer.addEventListener('change', colors);
-greenContainer.addEventListener('change', colors);
-blueContainer.addEventListener('change', colors);
-copyButton.addEventListener('click', copy);
+/* Os eventos "input" e "change" são semelhantes e funcionam, porém o primeiro é melhor para este caso */
+RSliderElement.addEventListener('input', getColorValues);
+GSliderElement.addEventListener('input', getColorValues);
+BSliderElement.addEventListener('input', getColorValues);
+RGBOutputCopyButton.addEventListener('click', copyRBG);
+HexOutputCopyButton.addEventListener('click', copyHex);
